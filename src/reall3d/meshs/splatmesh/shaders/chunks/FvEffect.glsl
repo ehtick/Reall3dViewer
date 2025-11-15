9e4914bdc7c251c18bef305374225476f15650b4
gotoeasy <gotoeasy@163.com>
2025-11-15 22:34:04 +0800

    fv

--- src/reall3d/meshs/splatmesh/shaders/chunks/FvEffect.glsl
+++ src/reall3d/meshs/splatmesh/shaders/chunks/FvEffect.glsl
@@ -2,7 +2,7 @@
 // Copyright (c) 2025 reall3d.com, MIT license
 // ==============================================
 vec4 FvEffect(uvec4 uv4Cen, vec4 v4Color, uint activeFv, float time) {
-    uint fvSplat = uv4Cen.w & 65535u;
+    uint fvSplat = uv4Cen.w >> 17u;
     if (fvSplat > 0u && fvSplat == activeFv) {
         return vec4(v4Color.rgb * fnWave(1.0, 1.6, time), v4Color.a);
     }
