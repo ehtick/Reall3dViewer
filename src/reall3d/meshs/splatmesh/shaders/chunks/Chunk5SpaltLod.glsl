65e17567a228e7c6dc8595729a594aeb69756d43
gotoeasy <gotoeasy@163.com>
2025-11-27 15:23:54 +0800

    lod

---
+++ src/reall3d/meshs/splatmesh/shaders/chunks/Chunk5SpaltLod.glsl
@@ -0,0 +1,20 @@
+// ==============================================
+// Copyright (c) 2025 reall3d.com, MIT license
+// ==============================================
+#ifdef BigSceneMode
+if (bigSceneMode && useLod) {
+int lod = int(cen.w >> 30u);
+float distance = length(v3Cen - cameraPosition);
+if (distance < 50.0) {
+if (lod < 2) {
+vColor = vec4(0.0);
+return;
+}
+} else if (distance > 80.0) {
+if (lod > 0) {
+vColor = vec4(0.0);
+return;
+}
+}
+}
+#endif
