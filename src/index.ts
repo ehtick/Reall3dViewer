df05f34a17e71d8023762aa5893e1b372f142542
gotoeasy <gotoeasy@163.com>
2026-01-28 21:35:55 +0800

    tpv

--- src/index.ts
+++ src/index.ts
@@ -27,8 +27,10 @@ if (url) {
 } else {
     viewer = new Reall3dViewer({ debugMode: true, maxRenderCountOfPc, shDegree });
     // viewer.addModel(`https://reall3d.com/demo-models/hornedlizard.v3.spx`);
+    // viewer.addModel(`https://reall3d.com/demo-models/yz.spx`);
 
-    viewer.addScene(`https://reall3d.com/demo-models/lod-v1/hgd/hgd-lod.scene.json`);
+    // viewer.addScene(`https://reall3d.com/demo-models/lod-v1/hgd/hgd-lod.scene.json`);
+    viewer.addScene(`https://reall3d.com/demo-models/lod-v1/hgd/hgd-lod-tpv.scene.json`);
 
     initDevMode();
 }
