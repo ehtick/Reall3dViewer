096bcc022d038d16877d7d2d3b5857ec718de986
gotoeasy <gotoeasy@163.com>
2026-04-05 12:16:35 +0800

    clean

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,7 +1,9 @@
 declare module '*.less' {
-  const classes: { [key: string]: string };
-  export default classes;
+    const classes: { [key: string]: string };
+    export default classes;
 }
 
 declare module '*.css';
 declare module '*.scss';
+
+declare module 'virtual:svg-icons-register';
