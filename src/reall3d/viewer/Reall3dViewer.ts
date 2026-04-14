ddb38cfe95448f12f5aef5ab12cddd06ad440c83
gotoeasy <gotoeasy@163.com>
2026-04-14 08:41:04 +0800

    clean

--- src/reall3d/viewer/Reall3dViewer.ts
+++ src/reall3d/viewer/Reall3dViewer.ts
@@ -212,7 +212,7 @@ export class Reall3dViewer {
         let minimapOpts: MinimapOptions;
         on(OnMetaDataLoaded, () => {
             minimapOpts = (fire(GetMeta) as MetaData).minimap;
-            if (isMobile) {
+            if (minimapOpts && isMobile) {
                 minimapOpts.size = minimapOpts.mobileSize || minimapOpts.size;
                 minimapOpts.corner = minimapOpts.mobileCorner || minimapOpts.corner || 'bottom-right';
                 minimapOpts.marginX = minimapOpts.mobileMarginX || 0;
